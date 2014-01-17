a5ce55e84bf782237e57ef74f9790ecd330ff596
jmgomez <juamangomalv@gmail.com>
2014-01-17 21:19:13 +0000

    first commit

---
+++ Library1.fs
@@ -0,0 +1,89 @@
+﻿namespace FSharpModule
+open System.Web.Mvc
+open Orchard.Themes
+open Orchard.ContentManagement
+open Orchard.ContentManagement.Records
+open System.ComponentModel.DataAnnotations
+open Orchard.ContentManagement.Handlers
+open Orchard.Data
+open Orchard.ContentManagement.Drivers
+open EkonBenefits.FSharp.Dynamic
+open Orchard.Data.Migration
+open Orchard.ContentManagement.MetaData
+open Orchard.ContentManagement.MetaData.Builders
+open Orchard.Core.Contents.Extensions
+open System.Data
+open FSharpModule.Models
+
+
+
+
+
+type Salute() =
+    member this.SayHello = "Hello from Fsharp"
+
+[<Themed>]
+type FSharpController() =
+    inherit Controller()        
+    member this.Index() =             
+        this.View(Salute())
+
+
+type SessionConfiguration() =
+    interface ISessionConfigurationEvents with 
+        member this.Created(cfg,defaultModel) = 
+          cfg.ExposeConfiguration(fun c->c.Properties.Add("use_proxy_validator", "false") |>ignore)|>ignore
+          ignore()
+        member this.Building(cfg) =
+          ignore()
+        member this.Prepared(cfg) =
+          ignore()
+        member this.Finished(cfg) =
+          ignore()
+        member this.ComputingHash(hash) =
+          ignore()
+
+type MapHandler(repository:IRepository<MapRecord>) = 
+    inherit ContentHandler()
+    do        
+        base.Filters.Add(StorageFilter.For(repository)) |> ignore
+        
+            
+type MapDriver() = 
+    inherit ContentPartDriver<MapPart>()
+    override this.Prefix with get() = "MapPart"
+    override this.Display(part,displayType,shapeHelper) =             
+        this.ContentShape("Parts_Map",fun()-> shapeHelper?Parts_Map(part)) 
+        :> DriverResult
+    //GET
+    override this.Editor(part,shapeHelper) =      
+        let editor = shapeHelper?EditorTemplate()       
+        editor?TemplateName <- "Parts/Map"
+        editor?Model <- part
+        editor?Prefix <- this.Prefix          
+        this.ContentShape("Parts_Map_Edit",fun()->editor):> DriverResult
+    //POST
+    override this.Editor(part,updater,shapeHelper) = 
+        updater.TryUpdateModel(part,this.Prefix,null,null) |> ignore
+        this.Editor(part,shapeHelper)
+
+
+type Migration() =
+    inherit DataMigrationImpl()
+    member this.Create()=                
+        this.SchemaBuilder.CreateTable("MapRecord",fun table->
+            table.ContentPartRecord().Column<double>("Latitude")
+            |> fun t->t.Column<double>("Longitude")
+            |>ignore)
+            |>ignore 
+        this.ContentDefinitionManager.AlterPartDefinition("MapPart",fun cfg->
+            cfg.Attachable()
+            |>ignore)
+            |>ignore
+        this.ContentDefinitionManager.AlterTypeDefinition("MapWidget",fun cfg->
+            cfg.WithPart("MapPart").WithPart("WidgetPart")
+            |> fun t->t.WithPart("CommonPart").WithSetting("Stereotype","Widget")
+            |>ignore)
+            |>ignore
+        1
+
---
+++ Models.fs
@@ -0,0 +1,31 @@
+﻿namespace FSharpModule.Models
+open Orchard.ContentManagement
+open Orchard.ContentManagement.Records
+open System.ComponentModel.DataAnnotations
+open System.Xml.Serialization
+type DVA = DefaultValueAttribute
+
+type MapRecord() = 
+    inherit ContentPartRecord()
+    [<DVA(false)>] val mutable private lat : double 
+    [<DVA(false)>] val mutable private lon : double 
+
+    abstract Latitude : double with get,set 
+    override this.Latitude with get() = this.lat
+                           and set(v) = this.lat <- v
+
+    abstract Longitude : double with get,set
+    override this.Longitude with get() = this.lon 
+                            and set(v) = this.lon <- v         
+type MapPart() = 
+    inherit ContentPart<MapRecord>()
+    [<Required>]
+    member this.Latitude 
+        with get() = base.Record.Latitude
+        and set(v) = base.Record.Latitude <- v
+    [<Required>]
+    member this.Longitude 
+        with get() = base.Record.Longitude
+        and set(v) = base.Record.Longitude <- v
+
+
